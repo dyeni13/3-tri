02acb966e944975db98e4b3812d957259be55c62
dyeni13 <dyenifertamaradossantos@gmail.com>
2024-11-29 12:17:34 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -2,20 +2,32 @@ criaCartao(
     categoria,
     pergunta,
     resposta,
-    )
-    criaCartao(
-        "Programação",
-        "O que é JavaScript?",
-        "O JavaScript é uma linguagem de programação"
-    )
-    criaCartao(
-        "Geografia",
-        "Qual a capital da França?"",
+)
+criaCartao(
+    "Programação",
+    "O que é JavaScript?",
+    "O JavaScript é uma linguagem de programação"
+)
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?"",
         "A capital da França é Paris"
 
-    )
-    criaCartao(
-        "Programação",
-        "O que é uma função?",
-        "Uma função é um bloco de código que executa alguma tarefa"
-    )
+)
+criaCartao(
+    "Programação",
+    "O que é uma função?",
+    "Uma função é um bloco de código que executa alguma tarefa"
+)
+    < footer >
+    <p>Projeto desenvolvido pela Alura, sem fins lucrativos</p>
+    <footer/>
+    <><script src app js></script><script src perguntas js>
+        </script></>
+    
+    </>
+    criarCartao(categoria, pergunta, resposta) {
+}
+function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta);
+}
